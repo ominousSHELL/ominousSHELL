aea9817eef428bba1b9fe99b6c05ed681027e727
ominousSHELL <ominousshellotto@proton.me>
2023-11-16 16:06:06 +0200

    1.0

--- ominousdjango/ominousnotes/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from ominousnotes.models import Notes
-
-admin.site.register(Notes)
-# Register your models here.
--- ominousdjango/ominousproject/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from ominousproject.models import Blog, Notes
-
-admin.site.register(Blog)
-admin.site.register(Notes)
