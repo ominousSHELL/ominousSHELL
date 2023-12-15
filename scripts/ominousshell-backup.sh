44a719339b3844d5f42b89244d8c74643bdfea11
ominousSHELL <ominousshellotto@proton.me>
2023-12-15 16:25:53 +0200

    1.0

---
+++ scripts/ominousshell-backup.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+
+BACKUP_DIR=/media/backup/
+
+echo "Backing up 'ominousSHELL' to SSD..."
+cp -r ~/ominousSHELL $BACKUP_DIR
+
+echo "Backing up 'ominousSHELL' to Github..."
+cd ~/ominousSHELL/
+git config --global user.email "ominousshellotto@proton.me"
+git add .
+git commit -m "1.0"
+git push
+
