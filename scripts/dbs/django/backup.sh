7948e54768184433edec602d2a47625ee9690314
root <root@ominousshell.ominousshell>
2024-02-23 22:38:47 +0200

    1.0

--- scripts/dbs/django/backup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-echo -e "Creating database backup file $1..."
-mysqldump -u root -panonymous ominousdb > ~/ominousSHELL/ominousdjango/backup.sql
-if [[ $? == 0 ]]; then
-	echo -e "Done"
-fi
